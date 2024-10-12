0f3afc4cfaac6b10abb3bf3ffdbc1b2396934e43
scheck93 <109469032+scheck93@users.noreply.github.com>
2024-10-12 00:14:11 +0000

    adding liability from frontend to back working

--- src/front/js/component/form.js
+++ src/front/js/component/form.js
@@ -1,16 +1,28 @@
 import React from "react";
 
-export default function Form({ label, form, type, showButton=false, buttonText, setValue }) {
-    
+export default function Form({ label, form, type, showButton=false, buttonText, setValue, value, onButtonClick }) {
   return (
-    
     <div className="d-flex justify-content-center">
       <div className="mb-3 col-5">
         <label htmlFor="formGroupExampleInput" className="form-label d-flex fw-semibold ps-2">{label}</label>
-        <input type={type} className="form-control shadow-sm border border-dark-subtle rounded-4 py-3" onChange={(e) => setValue(e.target.value)} id="formGroupExampleInput" placeholder={form} />
-        {showButton && <button type="button" class="btn btn-lg btn-secondary mt-5">{buttonText}</button>}        
+        <input 
+          type={type} 
+          className="form-control shadow-sm border border-dark-subtle rounded-4 py-3" 
+          onChange={(e) => setValue(e.target.value)} 
+          value={value}
+          id="formGroupExampleInput" 
+          placeholder={form} 
+        />
+        {showButton && (
+          <button 
+            type="button" 
+            className="btn btn-lg btn-secondary mt-5"
+            onClick={onButtonClick}
+          >
+            {buttonText}
+          </button>
+        )}        
       </div>
     </div>
-    
   );
 }
