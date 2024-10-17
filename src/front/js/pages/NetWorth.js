560395d059e730bd85311798239ea2a38dc2f69c
Garet Kean <167364720+GaretKean@users.noreply.github.com>
2024-10-17 00:50:02 +0000

    updated working from main

--- src/front/js/pages/NetWorth.js
+++ src/front/js/pages/NetWorth.js
@@ -13,7 +13,7 @@ export default function NetWorth() {
     const [liabilitiesData, setLiabilitiesData] = useState({ total: 0, lastUpdated: null });
     const [assetsData, setAssetsData] = useState({ total: 0, lastUpdated: null });
     const [showChart, setShowChart] = useState(false);
-    const { actions, store } = useContext(Context);
+    
     
     const calculateDuration = (amount) => {
         const baseDuration = 2;
